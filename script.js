930b38e8beadc5577c1b104bd42136caca2cfd0a
Suraj-Sharma001 <surajsharma60923@gmail.com>
2025-02-26 02:59:21 +0530

    responsiveness

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
-document.getElementById('btn').addEventListener('click', async () => {
-  const search = document.getElementById('search-input').value
-  if(!search){
-    console.log('Empty search!!')
+const output = document.getElementById("output");
+
+document.getElementById("btn").addEventListener("click", async () => {
+  const search = document.getElementById("search-input").value;
+  if (!search) {
+    output.innerHTML = "<b style='color: red'>Please enter your query!</b>";
+    output.style.display = "block";
+    console.log("Empty search!!");
     return;
-  }
-
-{apiKey}
-{api_url}
-  const res = await fetch(api_url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      contents: [{
-        parts: [{text: search}]
-      }]
-    })
-  })
-
-  const result_data = await res.json()
-
-  const output = document.getElementById('output')
-  if(result_data && result_data.candidates && result_data.candidates[0].content.parts[0].text) {
-    output.innerHTML = result_data.candidates[0].content.parts[0].text
   } else {
-    output.innerHTML = "No Result Found!!"
+    output.innerHTML = "<span>Searching...</span>";
+    output.style.display = "block";
   }
-})
+
+  const apiKey = ""
+  const api_url = ``
+
+
+  try {
+    const res = await fetch(api_url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        contents: [
+          {
+            parts: [{ text: search }],
+          },
+        ],
+      }),
+    });
+  
+    const result_data = await res.json();
+  
+    if (result_data?.candidates?.[0]?.content?.parts?.[0]?.text) {
+      output.innerHTML = `<b>${result_data.candidates[0].content.parts[0].text}</b>`;
+    } else {
+      output.innerHTML = "<b style='color: green'>No Result Found!!</b>";
+    }
+  } catch (error) {
+    output.innerHTML = "<b style='color: red'>Something went wrong, Please try again later!!</b>";
+  }
+});
